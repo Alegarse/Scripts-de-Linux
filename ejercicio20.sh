98ab9de50fe973043a4801d4d9e9135b5bcc9d06
Alejandro García Serrano <44372747+Alegarse@users.noreply.github.com>
2019-03-29 17:54:15 +0100

    Actualizado

--- ejercicio20.sh
+++ ejercicio20.sh
@@ -7,4 +7,20 @@ El script deberá verificar que se ha introducido algún parámetro, y en el
 caso de que se haya introducido, verificar antes de comprobar los
 permisos que el fichero existe.
 ENUNCIADO
-
+if [ $# -eq 0 ]
+then
+echo "No has introducido ningún parámetro"
+else
+  if [ -f $1 ]
+  then
+  echo "El fichero $1 ya existe"
+    if [ -x $1 ]
+    then
+    echo "El fichero $1 tiene permisos de ejecución"
+    else
+    echo "Elfichero $1 no tiene permisos de ejecución"    
+    fi
+  else
+  echo "El fichero $1 no existe"
+  fi
+fi
